7a625a34bcf19539868f96f4ddf38ae4808c8645
Jatinder Singh <xxxv.coin@gmail.com>
2015-02-06 22:53:58 -0500

    Adds support for centos + oracle jdk 8 + jboss eap 6.3

--- jboss/eap/6.3/build-image.sh
+++ jboss/eap/6.3/build-image.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-if [ ! -e files/jboss-eap-6.3.0.zip ]
-then
-   echo "You should get required EAP binary before trying to build this image."
-   echo "Binary is available at: https://access.redhat.com/jbossnetwork/restricted/softwareDownload.html?softwareId=32483&product=appplatform"
-   exit 255
-fi
-
-docker build --rm -t cointify/jboss-eap:6.3.0  .
+docker build -t cointify/jboss630 .
